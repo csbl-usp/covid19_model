b38dd79a79db66cdfb8cf0606d628abd5fe93b96
Juan Carlo <55853690+juan-css@users.noreply.github.com>
2020-03-14 17:28:41 -0300

    Writing the model (problem with the time column)
    Pessoal, atualmente estou nesse ponto escrevi as funções, acho que todas que o George tem na tabela dele. Mas já compartilho com vocês os códigos que fiz até agora...estou fazendo os códigos sempre reativos aos dados que o usuário pode colocar em "numericInput". Se alguém souber como me ajudar a transformar esses dados em gráficos como esses: (https://plot.ly/r/line-charts/) seria muito bom. Vou enviar a planilha que to usando pra vocês pelo whatsapp, eu comentei algumas células que podem ajudar a entender o que eu fiz. qualquer coisa estou a disposição e tentando fazer aqui!!

--- modelo_george/app.R
+++ modelo_george/app.R
@@ -1,9 +1,26 @@
 library(shiny)
 
-#Defining UI -----
+#Defining parameters
+alfa1 <- -257.580667091408
+alfa2 <- 67698.802805002
+beta <- 0.4375 #beta = gama*R0
+gama <- 0.15625
+R0 <- 2.8 #beta/gama
+R0_min <- 1.02
+
+#Define time
+time_range <- seq(from = 0, to = 3600, by = 0.01)
+
+#How can I get all the values 0, 0.01, 0.02, 0.03, 0.04.......? Like the column of time (Column A) of George's table.... 
+# for (i in time_range){
+#
+#   }
+
+#Defining UI ------
 ui <- fluidPage(titlePanel("Covid-19 Prediction Model"),
                 
-                sidebarLayout(                   #Total population
+                sidebarLayout(
+                  #Total population
                   sidebarPanel(
                     numericInput(
                       "total_pop",
@@ -11,16 +28,30 @@ ui <- fluidPage(titlePanel("Covid-19 Prediction Model"),
                       value = 21571281,
                       min = 1
                     ),
-                    numericInput(                #Infected number
+                    numericInput(
+                      #Infected number
                       "infected",
                       "Number of infected",
                       value = 151,
                       min = 1
                     ),
-                    numericInput(                #Recovered
+                    numericInput(
+                      #Recovered
                       "recovered",
                       "Number of recovered",
                       value = 0,
+                      min = 1
+                    ),
+                    numericInput(
+                      "quarantine_gov",
+                      "After the first case, which day the goverment will start the quarentine?",
+                      value = 15,
+                      min = 1
+                    ),
+                    numericInput(
+                      "time_to_start_quarantine_pop",
+                      "Assume that peolpe needs some time (days) to start the quarentine effectively:",
+                      value = 10,
                       min = 1
                     )
                   ),
@@ -31,18 +62,49 @@ ui <- fluidPage(titlePanel("Covid-19 Prediction Model"),
 
 #Defining server logic -----
 server <- function(input, output, session) {
-  susceptible_calc <- reactive({                                #Number of susceptibles
-    input$total_pop - input$infected - input$recovered
+  ########Defining rates
+  susceptible_calc <-
+    reactive({
+      #Number of susceptibles
+      input$total_pop - input$infected - input$recovered
+    })
+  
+  susceptible_rate <-
+    reactive({
+      #Rate of susceptibles
+      ((input$total_pop - input$infected - input$recovered) / input$total_pop)
+    })
+  
+  infected_rate <-
+    reactive({
+      #Rate of infected
+      input$infected / input$total_pop
+    })
+  
+  recovered_rate <-
+    reactive({
+      #rate of recovered
+      1 - input$infected_rate - input$susceptible_rate
+    })
+  
+  ######Defining predictions
+  susceptible_prediction <- reactive({
+    (input$susceptible_rate - gama) * R0 * input$susceptible_rate * infected_rate *
+      (time_range - 0)
   })
   
-  susceptible_percent <- reactive({                              #Rate of susceptibles
-   ((input$total_pop - input$infected - input$recovered) / input$total_pop)
+  infected_prediction <-
+    reactive({
+      input$infected_rate + 
+        (gama * R0 * input$susceptible_rate * input$infected_rate - gama * input$infected_rate) * (time_range - 0)
+    })
+  
+  recovered_prediction <- reactive({
+    1 - input$susceptible_prediction - input$infected_prediction
   })
   
-  infected_percent <- reactive({                              #Rate of infected
-    
-  })
   
+  #####Outputs
   output$susceptible <- renderText({
     print_susceptible_message <-
       paste("There are",
@@ -52,14 +114,14 @@ server <- function(input, output, session) {
     return(print_susceptible_message)
   })
   
-  output$suscept_percent <- renderText({
-    print_susceptible_percent <-
-      paste("This is",
-            susceptible_percent(),
-            "% of the total population.")
-
-    return(print_susceptible_percent)
-  })
+  # output$suscept_percent <- renderText({
+  #   print_susceptible_percent <-
+  #     paste("This is",
+  #           susceptible_percent(),
+  #           "% of the total population.")
+  #
+  #   return(print_susceptible_percent)
+  # })
   
 }
 
