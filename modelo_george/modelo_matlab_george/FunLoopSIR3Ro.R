43ed0e6f383fed18e7e7e7ea4411af6af44cbdce
Juan Carlo <55853690+juan-css@users.noreply.github.com>
2020-03-17 20:49:42 -0300

    Add files via upload

---
+++ modelo_george/modelo_matlab_george/FunLoopSIR3Ro.R
@@ -0,0 +1,23 @@
+FunLoopSIR3Ro <- function(Si, Ii, gamma,Ro1, Ro2,Ro3, iquarant,durationq, x){
+  beta1 <- Ro1*gamma;    beta2 <- Ro2*gamma; beta3 <- Ro3*gamma
+  N <- length(x)
+  S(1) <- Si;  I(1) <- Ii;  R(1) <- 1 - S(1) - I(1)
+  for (i in 1:N-1){
+    dt <- 10^-1
+    S(i+1) <- S(i) -  beta1*S(i)*I(i)*dt
+    I(i+1) <- 1 - S(i+1) - R(i) 
+    R(i+1) <- R(i) + gamma*I(i)*dt
+    if (i  > iquarant*100){
+      S(i+1) <- S(i) -  beta2*S(i)*I(i)*dt
+      I(i+1) <- 1 - S(i+1) - R(i) 
+      R(i+1) <- R(i) + gamma*I(i)*dt
+    }
+    if (i  > (iquarant+durationq)*100){
+      S(i+1) <- S(i) -  beta3*S(i)*I(i)*dt
+      I(i+1) <- 1 - S(i+1) - R(i) 
+      R(i+1) <- R(i) + gamma*I(i)*dt
+    }
+  }
+  # # Rout = downsample(R,steps);
+  Rout <- R
+}
